5f0ddee31ca262ce25bcfb85401966bd719d1929
KaiseiOhashi <131650527+KaiseiOhashi@users.noreply.github.com>
2023-07-29 14:46:27 +0900

    新しいフォルダ

---
+++ book/data.sql
@@ -0,0 +1,8 @@
+<script src="alasql.js"></script>
+<script>
+alasql("CREATE TABLE test (language INT, hello STRING)");
+alasql("INSERT INTO test VALUES (1,'Hello!')");
+alasql("INSERT INTO test VALUES (2,'Aloha!')");
+alasql("INSERT INTO test VALUES (3,'Bonjour!')");
+console.log( alasql("SELECT * FROM test WHERE language > 1") );
+</script>
