164a45c5877628a9cf7b94f99518773a64c52756
KaiseiOhashi <131650527+KaiseiOhashi@users.noreply.github.com>
2023-08-09 17:00:56 +0900

    Add files via upload

---
+++ bookadomin/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>蔵書管理システム</title>
+        <link rel="stylesheet" type="text/css" href="style.css">
+    </head>
+    <body>
+        <h1>蔵書管理システム</h1>
+
+        <!-- メニュー -->
+        <div class="menu">
+            <a ref="add_book.php">本の追加</a>
+            <a ref="add
+        </div>
+    </body>
+</html>
